8d23a5974b8ed2b9b277a91a9a5aad48dad42a43
Dvun <39218416+Dvun@users.noreply.github.com>
2021-06-04 17:54:40 +0300

    update

--- client/src/components/dashboard/room/CreateNewRoom.jsx
+++ client/src/components/dashboard/room/CreateNewRoom.jsx
@@ -32,6 +32,7 @@ const CreateNewRoom = () => {
         })
       })
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentRoom, candidates])
 
   // Create and Update Candidate
--- client/src/validation/inputsValidations.js
+++ client/src/validation/inputsValidations.js
@@ -3,6 +3,7 @@ import * as yup from 'yup'
 export const registerValidation = yup.object().shape({
   name: yup.string().required('Name is required!'),
   email: yup.string().required('Email is required!').email('Email not valid!')
+    //eslint-disable-next-line
     .matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email is not correct!'),
   password: yup.string().required('Password is required!').min(5, 'Minimum length is 5 chars').matches(/\d/, 'Password must contains a number!'),
   repeatPassword: yup.string().required('Password must match!').oneOf([yup.ref('password'), null], 'Password must match!')
@@ -10,6 +11,7 @@ export const registerValidation = yup.object().shape({
 
 export const loginValidation = yup.object().shape({
   email: yup.string().required('Email is required!').email('Email not valid!')
+    //eslint-disable-next-line
     .matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email is not correct!'),
   password: yup.string().required('Password is required!').min(5, 'Minimum length is 5 chars').matches(/\d/, 'Password must contains a number!'),
 })
